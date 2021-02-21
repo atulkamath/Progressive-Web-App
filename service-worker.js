d2c84c33767d8344d8d09e87d2832a92831e6d0b
Atul <realatulkamath@gmail.com>
2021-02-21 10:43:06 +0400

    PWA stage 1

--- service-worker.js
+++ service-worker.js
@@ -9,12 +9,30 @@ var cachefiles = [
   "images/images3.jpg",
 ];
 
-self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log("[Service Worker] caching files");
-      return cache.addAll(cachefiles);
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      // Download the file if it is not in the cache,
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          // add the new file to cache
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
     })
   );
 });
+
+// self.addEventListener("install", (e) => {
+//   console.log("[Service Worker] Install");
+//   e.waitUntil(
+//     caches.open(cacheName).then((cache) => {
+//       console.log("[Service Worker] caching files");
+//       return cache.addAll(cachefiles);
+//     })
+//   );
+// });
