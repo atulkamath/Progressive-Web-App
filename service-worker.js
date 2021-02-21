cc8dad7bb4fd301b849cb3f4b0bc16187484c657
Atul <realatulkamath@gmail.com>
2021-02-21 09:48:00 +0400

    Testing github pages

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+var cacheName = "petstore-v1";
+var cachefiles = [
+  "index.html",
+  "product.js",
+  "petstore.webmanifest",
+  "images/store-icon.jpg",
+  "images/images.jpg",
+  "images/images2.jpg",
+  "images/images3.jpg",
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] caching files");
+      return cache.addAll(cachefiles);
+    })
+  );
+});
