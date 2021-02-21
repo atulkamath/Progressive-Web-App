22ce247c3aa2296e47c5aebd483548073a192e12
Atul <realatulkamath@gmail.com>
2021-02-21 11:14:51 +0400

    Service code updated

--- service-worker.js
+++ service-worker.js
@@ -13,12 +13,21 @@ self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install");
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
-      console.log("[Service Worker] caching files");
-      return cache.addAll(cachefiles);
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
     })
   );
 });
-
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         // check if the cache has the file
+//         caches.match(e.request).then(function (r)
+//         {console.log('[Service Worker] Fetching resource: ' + e.request.url);
+//         // 'r' is the matching file if it exists in the cache
+//         return r
+//     })
+//     );
+// });
 self.addEventListener("fetch", function (e) {
   e.respondWith(
     caches.match(e.request).then(function (r) {
